dc920ca7341fa46e4806a5c6cf45d858296c90dd
JimmyDiaz13 <132846405+JimmyDiaz13@users.noreply.github.com>
2023-05-07 17:13:11 -0500

    hola mundo

---
+++ src/main/java/com/mycompany/talenttrade/TalentTrade.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.talenttrade;
+
+import modelo.Conexion;
+
+public class TalentTrade {
+
+    public static void main(String[] args) {
+        Conexion cxn=new Conexion();
+        cxn.getCon();
+    }
+}
